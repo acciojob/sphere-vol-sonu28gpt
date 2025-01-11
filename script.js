2ee5bf7e00961c48c67f0e203b17b34ce8d2d761
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-12 00:54:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+	e.preventDefault();
+	let r=document.getElementById('radius').value;
+	const v=document.getElementById('volume');
+	r=+r;
+	// alert(r);
+	if(isNaN(r)|| r<0){
+		v.value=NaN;
+		return ;
+	}
+	let ans=(Math.PI)*(Math.pow(r,3))*4/3;
+	// alert(ans);
+	v.value=ans.toFixed(4);
+	
+	// return ;
   
 } 
 
